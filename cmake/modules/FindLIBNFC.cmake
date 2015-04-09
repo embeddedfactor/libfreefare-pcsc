08f36d19ac29410ff99433a0fe8bf2d87d8fa6e3
Christoph Szeppek <cszeppek@embeddedfactor.com>
2015-04-09 10:03:58 +0200

    modified cmake to find libnfc on linux OSs

--- cmake/modules/FindLIBNFC.cmake
+++ cmake/modules/FindLIBNFC.cmake
@@ -10,7 +10,11 @@ message("libnfc install dir: " ${LIBNFC_INSTALL_DIR})
 find_path(LIBNFC_INCLUDE_DIRS NAMES nfc/nfc.h PATHS ${LIBNFC_INSTALL_DIR}/include)
 message("libnfc include dir found:  " ${LIBNFC_INCLUDE_DIRS})
 
-find_library(LIBNFC_LIBRARIES libnfc PATHS ${LIBNFC_INSTALL_DIR}/lib)
+if(CMAKE_SYSTEM_NAME MATCHES ’Linux’)
+  find_library(LIBNFC_LIBRARIES libnfc.so PATHS ${LIBNFC_INSTALL_DIR}/lib)
+else()
+  find_library(LIBNFC_LIBRARIES libnfc PATHS ${LIBNFC_INSTALL_DIR}/lib)
+endif()
 message("libnfc library found:  " ${LIBNFC_LIBRARIES})
 
 INCLUDE(FindPackageHandleStandardArgs)
